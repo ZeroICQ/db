2803e9a7a1551d9d89adbd0eb93e846499104240
Alexey Mogilyovkin <zeroicq@gmail.com>
2017-09-27 22:26:57 +1000

    Add data

--- Sql/groups.sql
+++ Sql/groups.sql
@@ -4,3 +4,13 @@ CREATE TABLE groups(
     id   INTEGER,
     name VARCHAR(255)
 );
+
+INSERT INTO groups VALUES(1, 'B8203a');
+
+INSERT INTO groups VALUES(2, 'B8203b');
+
+INSERT INTO groups VALUES(3, 'B8116');
+
+INSERT INTO groups VALUES(4, 'C1422');
+
+INSERT INTO groups VALUES(5, 'B5678');
