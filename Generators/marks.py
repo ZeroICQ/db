1245f6eed4e7bc530ebc8697871a2ba32052245c
Alexey Mogilyovkin <zeroicq@gmail.com>
2017-09-28 21:56:18 +1000

    Add id to marks generator

--- Generators/marks.py
+++ Generators/marks.py
@@ -4,7 +4,7 @@ import random
 sql = []
 
 for x in range(1000):
-    sql.append('INSERT INTO marks VALUES (%d, %d, %d);' % (random.randint(1, 100), random.randint(1, 58), random.randint(1, 5)))
+    sql.append('INSERT INTO marks VALUES (%d, %d, %d, %d);' % (x+1, random.randint(1, 100), random.randint(1, 58), random.randint(1,10)))
 
 with open('marks.txt', 'w') as f:
     f.write('\n'.join(sql))
