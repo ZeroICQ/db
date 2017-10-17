d621ac8e202bb3f45ceef963880a5180055fc4ef
Alexey Mogilyovkin <zeroicq@gmail.com>
2017-10-17 22:55:58 +1000

    Add 3rd homework

--- Homework/3.sql
+++ Homework/3.sql
@@ -12,3 +12,28 @@ where
     m.id is NUll
     and 
     s.id =21
+
+---готово--
+select
+    list(birtmonth),
+    passed
+from
+    (select
+        list(distinct extract(month from s.BIRTHDAY)) as birtmonth,
+        count(*) as passed
+    from STUDENTS s
+    inner join GROUPS g 
+        on g.ID = s.GROUP_ID
+    inner join SUBJECTS sub 
+        on sub.GROUP_ID = g.ID
+    left join MARKS m 
+        on m.SUBJECT_ID = sub.ID and m.STUDENT_ID = s.ID
+    where
+        m.id is NUll
+    group by
+        s.NAME, s.ID
+    )
+group by 
+    passed
+order by 
+    passed DESC
